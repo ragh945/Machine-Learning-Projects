b2f8c903ad691ace25ff66b4108855272257fd46
Patwari Raghottam <65483520+ragh945@users.noreply.github.com>
2025-04-15 11:22:05 +0530

    Add files via upload

---
+++ Estimation of Time Arrival/Estimation.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import sklearn
+from sklearn.linear_model import LinearRegression
+import pickle
+from PIL import Image
+
+model = model=pickle.load(open(r"estimator.pkl","rb"))
+image = Image.open(r"Inno.png")  # Ensure this path is correct
+
+# Display the image
+st.image(image, caption='Sample Image', use_column_width=True)
+st.title("Estimation of Time ML Project")
+st.subheader("By Patwari Raghottam")
+start_lat = st.number_input("Enter the start latitude:",)
+start_lang = st.number_input("Enter the start longitude:")
+end_lat = st.number_input("Enter the destination latitude:")
+end_lang = st.number_input("Enter the destination longitude:")
+dist = st.number_input("Enter the distance:")
+density = st.number_input("Enter the density:")
+weather = st.text_input("Enter the weather condition:")
+day  = st.number_input("Enter the day:")
+hour = st.number_input("Enter the hour:")
+
+weather_numerical = (1 if weather == "rainy" else 2 if weather == "foggy" else 3)
+if st.button("Submit"):
+    time = model.predict([[start_lat,start_lang ,end_lat,end_lang,dist,density,weather_numerical,day,hour]])[0]
+    st.write("The Estimated time is",time,'minutes')
+
+
+
